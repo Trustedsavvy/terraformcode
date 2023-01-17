db0c99f7d0e36db23aeb2e676543475ac66b873f
Trustedsavvy <118703403+Trustedsavvy@users.noreply.github.com>
2023-01-17 06:48:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
   features {}
-
-  subscription_id = "39495ab3-3382-43b6-af76-66afb0664184"
-  client_id       = "3d935813-c66f-4378-9cef-b19fec464dec"
-  client_secret   = "8Aw8Q~2oAphg6SkMRoJn9JxJd1oXoyJMP.36xaPD"
-  tenant_id       = "8fa50c11-53db-4b43-9069-99cf2b01788c"
 }
 
 terraform {
